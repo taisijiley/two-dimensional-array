e817dba1314d51d0462e18dc4a38ddc0489b5f72
Teren <Official.Teren@yandex.ru>
2024-05-04 01:18:52 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <ctime>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+/*ƒвумерный массив*/
+
+/*
+	col
+row[0.0][0.1][0.2]
+   [1.0][1.1][1.2]	 
+   [2.0][2.1][2.2]
+
+   [(row)1.(col)1]
+
+   1)обращение - a[0][0]
+   2)обращение - a[1][2]
+   3)обращение - a[0][4]
+*/
+
+int main()
+{
+
+}
